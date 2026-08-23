ea4bc0d0d799670172bacb789343f8777d8356dc
Faridun <elmurodovfaridun24@gmail.com>
2026-08-23 12:13:05 -0700

    add match/mismatch tests

--- tests/test_baseline.py
+++ tests/test_baseline.py
@@ -59,3 +59,86 @@ def test_amount_mismatch_returns_no_candidate():
     )
 
     assert len(result) == 0
+
+def test_currency_mismatch_returns_no_candidate():
+    invoice = Invoice(
+        id=uuid4(),
+        supplier_name="ABC",
+        invoice_number="2e123",
+        invoice_date=date(2026, 7, 1),
+        amount=Decimal("1250.00"),
+        currency="USD",
+    )
+    transaction = BankTransaction(
+        id=uuid4(),
+        transaction_date=date(2026, 7, 4),
+        description="Some",
+        amount=Decimal("-1250.00"),
+        currency="TJS",
+    )
+
+    result = find_candidate_matches(
+        invoices=[invoice],
+        transactions=[transaction]
+    )
+
+    assert len(result) == 0
+
+def test_transaction_before_invoice_returns_no_candidate():
+    invoice = Invoice(
+        id=uuid4(),
+        supplier_name="ABC",
+        invoice_number="2e123",
+        invoice_date=date(2026, 7, 1),
+        amount=Decimal("1250.00"),
+        currency="USD",
+    )
+    transaction = BankTransaction(
+        id=uuid4(),
+        transaction_date=date(2026, 5, 4),
+        description="Some",
+        amount=Decimal("-1250.00"),
+        currency="USD",
+    )
+
+    result = find_candidate_matches(
+        invoices=[invoice],
+        transactions=[transaction]
+    )
+
+    assert len(result) == 0
+
+def test_multiple_matching_invoices_return_multiple_candidates():
+    invoice_1 = Invoice(
+        id=uuid4(),
+        supplier_name="ABC",
+        invoice_number="2e123",
+        invoice_date=date(2026, 7, 1),
+        amount=Decimal("1250.00"),
+        currency="USD",
+    )
+    invoice_2 = Invoice(    
+        id=uuid4(),
+        supplier_name="ABCDE",
+        invoice_number="2e123d",
+        invoice_date=date(2026, 7, 1),
+        amount=Decimal("1250.00"),
+        currency="USD",
+    )
+
+    transaction = BankTransaction(
+        id=uuid4(),
+        transaction_date=date(2026, 7, 8),
+        description="Some",
+        amount=Decimal("-1250.00"),
+        currency="USD",
+    )
+
+    result = find_candidate_matches(
+        invoices=[invoice_1, invoice_2],
+        transactions=[transaction]
+    )
+
+    assert len(result) == 2
+
+
